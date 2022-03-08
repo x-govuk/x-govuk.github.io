0701e682cbfea1c0f87b8c0283ccbfefd93e4d09
Frankie Roberto <frankie@frankieroberto.com>
2022-03-08 14:26:43 +0000

    Add plugin config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
+const govukEleventyPlugin = require('govuk-eleventy-plugin')
+
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPlugin(govukEleventyPlugin, {
+  })
 
   return {
     dataTemplateEngine: 'njk',
