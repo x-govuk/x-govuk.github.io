7064ae0319f90ba03fc48987607149ecca5c87da
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-03-10 23:48:16 +0000

    Customised header and simplify layout inheritence

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const fs = require('node:fs');
 const govukEleventyPlugin = require('govuk-eleventy-plugin')
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(govukEleventyPlugin)
+  eleventyConfig.addPlugin(govukEleventyPlugin, {
+    organisationLogo: fs.readFileSync('./assets/logo.svg', 'utf8'),
+    organisationName: 'X-GOVUK'
+  })
 
   return {
     dataTemplateEngine: 'njk',
