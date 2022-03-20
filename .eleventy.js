e49d03a3aa04fe2e8edbb52a857e688c1eb53ce9
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-03-20 23:49:05 +0000

    Link to logo assets

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(govukEleventyPlugin, {
     brandColour: '#28a',
     fontFamily: 'system-ui, sans-serif',
+    icons: {
+      mask: 'https://raw.githubusercontent.com/x-govuk/logo/f27cadde0d65ea8d8355fe07eef4988cd1b16c1e/images/x-govuk-mask-icon.svg?raw=true',
+      shortcut: 'https://github.com/x-govuk/logo/blob/f27cadde0d65ea8d8355fe07eef4988cd1b16c1e/images/x-govuk-favicon.ico?raw=true',
+      touch: 'https://github.com/x-govuk/logo/blob/f27cadde0d65ea8d8355fe07eef4988cd1b16c1e/images/x-govuk-apple-touch-icon.png?raw=true'
+    },
     stylesheets: [
       '/assets/styles/application.css'
     ],
