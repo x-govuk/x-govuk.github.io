d6a73668541630d93e16c919417afeb870bcf85d
Frankie Roberto <frankie@frankieroberto.com>
2024-03-22 14:01:44 +0000

    Add RSS feed (#109)

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,10 @@
 const govukEleventyPlugin = require('@x-govuk/govuk-eleventy-plugin')
+const rssPlugin = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addPlugin(rssPlugin);
+
   eleventyConfig.addPlugin(govukEleventyPlugin, {
     icons: {
       mask: 'https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-mask-icon.svg?raw=true',
@@ -11,12 +15,21 @@ module.exports = function (eleventyConfig) {
     },
     opengraphImageUrl:
       'https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-opengraph-image.png',
+    feedUrl: 'https://x-govuk.github.io/posts.xml',
     homeKey: 'X-GOVUK',
     url: process.env.GITHUB_ACTIONS && 'https://x-govuk.github.io/',
     header: {
       logotype: 'x-govuk'
     },
     footer: {
+      meta: {
+        items: [
+          {
+            href: "/feed.xml",
+            text: "Subscribe to feed"
+          }
+        ]
+      },
       contentLicence: {
         html: 'An unofficial community project. <a class="govuk-footer__link" href="https://github.com/x-govuk/x-govuk.github.io">GitHub source</a>.'
       },
