6cea04435e9c8dacf8cd63335d96321ecfdc3400
Frankie Roberto <frankie@frankieroberto.com>
2025-05-21 12:32:42 +0100

    Fetch up to 100 repos to look for projects to display (#164)

--- app/data/repos.js
+++ app/data/repos.js
@@ -1,7 +1,7 @@
 import EleventyFetch from '@11ty/eleventy-fetch'
 
 export default async function () {
-  const url = 'https://api.github.com/orgs/x-govuk/repos'
+  const url = 'https://api.github.com/orgs/x-govuk/repos?per_page=100'
 
   return EleventyFetch(url, {
     duration: '1d',
