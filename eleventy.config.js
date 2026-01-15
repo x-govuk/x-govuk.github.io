49d4b71aa2125a2168e90ce7d2d566455bda8f7a
Frankie Roberto <frankie@frankieroberto.com>
2026-01-15 10:29:09 +0000

    use-https-with-github-domain

--- eleventy.config.js
+++ eleventy.config.js
@@ -55,7 +55,7 @@ export default function (eleventyConfig) {
     },
     themeColor: '#2288aa',
     titleSuffix: serviceName,
-    url: process.env.GITHUB_ACTIONS && 'http://x-govuk.github.io/'
+    url: process.env.GITHUB_ACTIONS && 'https://x-govuk.github.io/'
   })
 
   // Collections
